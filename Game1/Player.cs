acd97d636c9c9d12b40abc23c7adc96f9ad383a5
Mearwen <dami92@gmail.com>
2015-04-17 11:23:14 +0200

    v0.0
    Początek programu
    *ruch gracza
    /zaczęte
    animacje, ekrany gry

---
+++ Game1/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace PTM
+{
+    class Player
+    {
+        Texture2D playerSprite;
+        SpriteFont font;
+        Vector2 position;
+        Rectangle playerRect;
+        float moveSpeed = 500;
+        float jumpSpeed = 1000;
+        bool jump = false;
+        Vector2 velocity;
+        const float gravity = 40f;
+
+        KeyboardState keyState;
+        public void Initialize()
+        {
+            position = velocity = Vector2.Zero;
+        }
+        public void LoadContent(ContentManager content)
+        {
+            playerSprite = content.Load<Texture2D>("PlayerSprite");
+            font = content.Load<SpriteFont>("SpriteFont1");
+        }
+        public void Update(GameTime gameTime)
+        {
+            keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Keys.Right))
+                position.X += moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else if (keyState.IsKeyDown(Keys.Left))
+                position.X -= moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+            if (keyState.IsKeyDown(Keys.Up) && jump)
+            {
+                velocity.Y -= jumpSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                jump = false;
+            }
+            if (!jump)
+                velocity.Y += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else
+                velocity.Y = 0;
+            position += velocity;
+
+            jump = position.Y >= 350;
+            if (jump)
+                position.Y = 350;
+            
+            CheckBorders();
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(playerSprite, position, Color.White);
+            spriteBatch.DrawString(font, "X: " + position.X.ToString() + " Y: " + position.Y.ToString()
+                + "\nJump: " + jump.ToString() + " Up: " + keyState.IsKeyDown(Keys.Up).ToString(), Vector2.Zero, Color.White);
+        }
+        private void CheckBorders()
+        {
+            if (position.X < 0)
+                position.X = 0;
+            if (position.Y < 0)
+                position.Y = 0;
+            if (position.X + playerSprite.Height > 600)
+                position.X = 600 - playerSprite.Height;
+            if (position.Y + playerSprite.Width > 400)
+                position.Y = 400 - playerSprite.Width;
+            
+                  
+        }
+    }
+}
---
+++ Game1/ScreenManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PTM
+{
+    class ScreenManager
+    {
+        #region Variables
+
+        //kontent menadżer
+        ContentManager content;
+
+        //instancja menadżera
+        private static ScreenManager instance;
+
+        //Kolejność ekranów
+        Stack<GameScreen> screenStack = new Stack<GameScreen>();
+        
+        //Wymiary ekranu
+        Vector2 dimensions;
+
+        bool transition;
+        FadeAnimation fade;
+        Texture2D fadeTexture;
+
+
+        GameScreen currentScreen;
+        GameScreen newScreen;
+
+        #endregion
+        #region Properties
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ScreenManager();
+                return instance;
+            }
+        }
+
+        public Vector2 Dimensions
+        {
+            get { return dimensions; }
+            set { dimensions = value; }
+        }
+        #endregion
+        #region Main Methods
+
+        public void AddScreen(GameScreen screen)
+        {
+            transition = true;
+            newScreen = screen;
+            fade.IsActive = true;
+            fade.Alpha = 0.0f;
+            fade.ActiateValue = 1.0f;
+
+        }
+        public  void Initialize()
+        {
+            currentScreen = new SplashScreen();
+            fade = new FadeAnimation();
+        }
+        public void LoadContent(ContentManager Content)
+        {
+            content = new ContentManager(Content.ServiceProvider, "Content");
+            currentScreen.LoadContent(Content);
+
+            fadeTexture = content.Load<Texture2D>("fade");
+            fade.LoadContent(content, fadeTexture, "", Vector2.Zero);
+            fade.Scale = dimensions.X;
+        }
+        public void Update(GameTime gameTime) 
+        {
+            if (!transition)
+                currentScreen.Update(gameTime);
+            else
+                Transition(gameTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            currentScreen.Draw(spriteBatch);
+            if (transition)
+                fade.Draw(spriteBatch);
+        }
+        #endregion
+
+        #region private methods
+        private void Transition(GameTime gameTime)
+        {
+            fade.Update(gameTime);
+            if (fade.Alpha == 1.0f && fade.Timer.TotalSeconds == 1.0f)
+            {
+                screenStack.Push(newScreen);
+                currentScreen.UnloadContent();
+                currentScreen = newScreen;
+                currentScreen.LoadContent(content);
+            }
+            else if (fade.Alpha == 0.0f)
+            {
+                transition = false;
+                fade.IsActive = false;
+            }
+        }
+        #endregion
+    }
+}
